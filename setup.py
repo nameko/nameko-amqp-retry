7ae6b66b459cc0c5415f68d24a073ec0a346ca13
timbu <tim.buckland@student.com>
2017-06-01 15:06:58 +0100

    bump version and add changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='nameko-amqp-retry',
-    version='0.3.3',
+    version='0.3.4',
     description='Nameko extension allowing AMQP entrypoints to retry later',
     author='Student.com',
     url='http://github.com/nameko/nameko-amqp-retry',
